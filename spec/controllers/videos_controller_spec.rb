87349c0c90212a5bd3a40fae075d22da78732b26
Alex Slepak <alex_slepak_99@hotmail.com>
2013-08-08 13:30:50 -0700

    implement sign_in for tests

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe VideosController do
   describe "GET #new" do
     it "returns success" do
+      sign_in
       get :new
       expect(response.code).to eq("200")
     end
@@ -29,6 +30,7 @@ describe VideosController do
     let(:description) { "Oh, the Places You'll Go at Burning Man!" }
 
     it "creates a new Video" do
+      sign_in
       params = { :video => { :url => url,
                              :description => description }}
       expect { post :create, params }.to change(Video, :count).by(1)
